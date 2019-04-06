34a09cac176b1c14fc179b56c8132bd40858188d
unknown <rikhilcooldude@gmail.com>
2019-04-06 15:19:16 +0200

    docs added

--- src/app/custom-dropdown/dropdown.component.ts
+++ src/app/custom-dropdown/dropdown.component.ts
@@ -10,32 +10,45 @@ import { DropDownValue } from './dropdown.interface';
 export class DropDownComponent implements OnInit {
   @Input() 
   public dropDownValues: Array<DropDownValue>;
-  public dropDownOpened: boolean = false;
   @Input()
-  public defaultValue: DropDownValue;
-  public selectedValue: DropDownValue = {title: "Please Select a value"};
+  public defaultValue: DropDownValue = {title: "Please Select a value"}; // Default value 
 
+  public dropDownOpened: boolean = false;
+  public selectedValue: DropDownValue ;
+/**
+ * On init Lifecycle event
+ */
   public ngOnInit(): void {
-    if( this.defaultValue ){
-      this.selectedValue = this.defaultValue;
-    }
+    this.selectedValue = this.defaultValue; // sets the default value
   }
-
+/**
+ * Toggles the dropdown list
+ */
   public toggleDropDown(): void {
     this.dropDownOpened = !this.dropDownOpened
   }
-
+/**
+ * Forcefully closes the dropdown
+ */
   public closeDropDown(): void {
     this.dropDownOpened = false;
   }
-
+/**
+ * Set the selected value of the dro down
+ * @param selected {DropdownValue} selected value
+ */
   public setSelectedValue(selected: DropDownValue): void {
     this.selectedValue = selected;
     this.toggleDropDown();
   }
-
+/**
+ * Handle arrow movement
+ * @param event {KeyboardEvent} event
+ */
   public arrowMovement(event: KeyboardEvent): void {
-    console.log("moving")
+    //TODO
+    console.log("moving");
+
   }
 
 }
