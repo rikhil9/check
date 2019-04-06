b564ee4227f25a57b9ce8f3655bdf746fc3636a1
unknown <rikhilcooldude@gmail.com>
2019-04-06 14:36:54 +0200

    list style improved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DropDownComponent } from './custom-dropdown/dropdown.component';
 import { ClickOutsideModule } from './click-outside/clickOutside.module';
 import { ClickEscapeModule } from './click-escape/click-escape.module';
+import { DropDownComponent } from './custom-dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
